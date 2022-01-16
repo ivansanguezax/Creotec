f3b278c01afda212f4a76275b321d70aef3a7e10
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2022-01-16 19:53:30 -0400

    modular animations

--- javascript/index.js
+++ javascript/index.js
@@ -1,37 +1,44 @@
-var counter = 1;
-var counter2 = 1;
-var requestAnimationFrame = window.requestAnimationFrame 
-                            || window.mozRequestAnimationFrame 
-                            || window.webkitRequestAnimationFrame 
-                            || window.msRequestAnimationFrame
-                            || function(f){return setTimeout(f, 1000/60)};
+import AnimationCicle from "./modules/AnimationCicle.js";
 
-function movediv(){
-    setTimeout(function(){ //throttle requestAnimationFrame to 20fps
-        imgChange().next();
-        requestAnimationFrame(movediv);
-    }, 5000)
-}
+window.counter = 0;
+window.counter2 = 0;
 
-function* imgChange(){
-    while (true) {
-        if (counter == 5)counter = 1;
-        // TODO: Toggle images
-        $(`#btn-nav-img-${counter}`).toggleClass('homepage-images-btn');
-        $(`#btn-nav-img-${counter==4?1:counter+1}`).toggleClass('homepage-images-btn');
-        // TODO: Toggle images
-        $(`#card-${counter-1}`).toggleClass('services-card-active');
-        $(`#card-${counter==4?0:counter}`).toggleClass('services-card-active');
-        
-        if (counter2 == 6)counter2 = 1;
-        $(`#nav-service-item-${counter2}`).toggleClass('patients-nav-item');
-        $(`#nav-service-item-${counter2==5?1:counter2+1}`).toggleClass('patients-nav-item');
-        
-        
-        counter++;
-        counter2++;
-        yield
-    }
-}
+window.animateImages = new AnimationCicle(window.counter, 4, (counter) => {
+    $(`#btn-nav-img-${counter+1}`).toggleClass('homepage-images-btn');
+    $(`#btn-nav-img-${counter==3?1:counter+2}`).toggleClass('homepage-images-btn');
+},1000);
 
-movediv();
+
+window.animateImages.movediv();
+
+// function movediv(){
+//     setTimeout(function(){ //throttle requestAnimationFrame to 20fps
+//         imgChange().next();
+//         requestAnimationFrame(movediv);
+//     }, 5000)
+// }
+
+
+// function* imgChange(){
+//     while (true) {
+//         if (counter == 4)counter = 1;
+//         // TODO: Toggle images
+//         $(`#btn-nav-img-${counter+1}`).toggleClass('homepage-images-btn');
+//         $(`#btn-nav-img-${counter==3?1:counter+2}`).toggleClass('homepage-images-btn');
+        
+//         // TODO: Toggle images
+//         $(`#card-${counter}`).toggleClass('services-card-active');
+//         $(`#card-${counter==3?0:counter+1}`).toggleClass('services-card-active');
+        
+//         if (counter2 == 6)counter2 = 1;
+//         $(`#nav-service-item-${counter2+1}`).toggleClass('patients-nav-item');
+//         $(`#nav-service-item-${counter2==5?1:counter2+2}`).toggleClass('patients-nav-item');
+        
+        
+//         counter++;
+//         counter2++;
+//         yield
+//     }
+// }
+
+// movediv();
---
+++ javascript/modules/AnimationCicle.js
@@ -0,0 +1,34 @@
+class AnimationCicle{
+    constructor(counter, max, callback, timeout){
+        this.counter = counter;
+        this.max = max;
+        this.callback = callback;
+        this.timeout = timeout;
+
+        this.movediv = this.movediv.bind(this);
+        this.imgChange = this.imgChange;
+        this.requestAnimationFrame = window.requestAnimationFrame 
+                            || window.mozRequestAnimationFrame 
+                            || window.webkitRequestAnimationFrame 
+                            || window.msRequestAnimationFrame
+                            || function(f){return setTimeout(f, 1000/60)};
+    }
+
+    movediv(){
+        setTimeout(() => { //throttle requestAnimationFrame to 20fps
+            this.imgChange().next();
+            this.requestAnimationFrame.call(window,this.movediv);
+        }, this.timeout);
+    }
+
+    * imgChange(){
+        while(true){
+            this.counter = this.counter == this.max ? 0:this.counter;
+            this.callback(this.counter);
+            this.counter++;
+            yield
+        }
+    }
+}
+
+export default AnimationCicle;
