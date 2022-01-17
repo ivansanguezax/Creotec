65d1d6645a8c59a4f67008a2d580718424417e7e
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2022-01-16 20:10:53 -0400

    homepage animations

--- javascript/index.js
+++ javascript/index.js
@@ -1,44 +1,23 @@
 import AnimationCicle from "./modules/AnimationCicle.js";
 
+// counters
 window.counter = 0;
 window.counter2 = 0;
 
-window.animateImages = new AnimationCicle(window.counter, 4, (counter) => {
+// Creating functions
+window.animateImagesandCards = new AnimationCicle(window.counter, 4, (counter) => {
     $(`#btn-nav-img-${counter+1}`).toggleClass('homepage-images-btn');
     $(`#btn-nav-img-${counter==3?1:counter+2}`).toggleClass('homepage-images-btn');
+    $(`#card-${counter}`).toggleClass('services-card-active');
+    $(`#card-${counter==3?0:counter+1}`).toggleClass('services-card-active');
 },1000);
 
+window.animateCarrousel = new AnimationCicle(window.counter2, 5, (counter) => {
+    $(`#nav-service-item-${counter+1}`).toggleClass('patients-nav-item');
+    $(`#nav-service-item-${counter==4?1:counter+2}`).toggleClass('patients-nav-item');
+},2000);
 
-window.animateImages.movediv();
+// Calling function
+window.animateImagesandCards.movediv();
+window.animateCarrousel.movediv();
 
-// function movediv(){
-//     setTimeout(function(){ //throttle requestAnimationFrame to 20fps
-//         imgChange().next();
-//         requestAnimationFrame(movediv);
-//     }, 5000)
-// }
-
-
-// function* imgChange(){
-//     while (true) {
-//         if (counter == 4)counter = 1;
-//         // TODO: Toggle images
-//         $(`#btn-nav-img-${counter+1}`).toggleClass('homepage-images-btn');
-//         $(`#btn-nav-img-${counter==3?1:counter+2}`).toggleClass('homepage-images-btn');
-        
-//         // TODO: Toggle images
-//         $(`#card-${counter}`).toggleClass('services-card-active');
-//         $(`#card-${counter==3?0:counter+1}`).toggleClass('services-card-active');
-        
-//         if (counter2 == 6)counter2 = 1;
-//         $(`#nav-service-item-${counter2+1}`).toggleClass('patients-nav-item');
-//         $(`#nav-service-item-${counter2==5?1:counter2+2}`).toggleClass('patients-nav-item');
-        
-        
-//         counter++;
-//         counter2++;
-//         yield
-//     }
-// }
-
-// movediv();
