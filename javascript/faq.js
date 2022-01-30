bd2e036b25d5c6262af968e3f84f7a536fa853e7
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2022-01-30 04:53:35 -0400

    modals

--- javascript/faq.js
+++ javascript/faq.js
@@ -8,3 +8,15 @@ document.querySelector('#txtSearch').addEventListener('keypress', function (e) {
         s.changePage("./faq_research.html",{search: document.querySelector('#txtSearch').value});
     }
 });
+
+// TODO: Hide magnifing glass when typing
+// document.querySelector('#txtSearch').addEventListener('focus', () => {
+//     console.log('Focus');
+//     $('.form-control').css('background-image','');
+// });
+
+// document.querySelector('#txtSearch').addEventListener('blur', () => {
+//     console.log('Lost Focus');
+//     $('.form-control').css('background-image',"./assets/png/Search.png");
+// });
+
